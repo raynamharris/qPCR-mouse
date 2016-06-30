bde8461e8be386bacec3bdf777e6ac9bb8e7e183
Rayna Harris <rayna.harris@utexas.edu>
2016-06-30 14:50:19 -0400

    update script basic info

---
+++ Rayna/bin/mcmc.qpcr.rmh.R
@@ -0,0 +1,7 @@
+#=============================================================================================================#
+# Script created by Rayna Harris, rayna.harris@utexas.edu
+# Script created in version R 3.3.1 
+# This script is for analyzing data related to the mouse qpcr projects
+#=============================================================================================================#
+
+setwd("Z:/NSB_2016/IntegrativeNeuroscience/qPCR-mouse/Rayna/bin")
