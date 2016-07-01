6026af28460ec15ad9a0c3c8f75f7728958689fe
Rayna Harris <rayna.harris@utexas.edu>
2016-06-30 20:34:08 -0400

    updtaed script

--- Rayna/bin/mcmc.qpcr.rmh.R
+++ Rayna/bin/mcmc.qpcr.rmh.R
@@ -6,15 +6,19 @@
 
 ##script stored in "Z:/NSB_2016/IntegrativeNeuroscience/qPCR-mouse/Rayna/bin"
 ## set path to data dir
-setwd("Z:/NSB_2016/IntegrativeNeuroscience/qPCR-mouse/Rayna/data")
+setwd("Z:/NSB_2016/IntegrativeNeuroscience/qPCR-mouse/Rayna/data_xlxs")
 
 ## The process:
 ## 1. Loop over all experimental prjoect files and create one big "rawdata" dataframe
 ## 2. Clean the data to make numbers numbers and rename important columns
-## 3. Wrangle standard curve data with dplyr and plyr package into a dataframe called dilutions
+## 3. Create dilutions dataframe with quantiry, target name, and ct.
 ## 4. Calculate primer efficiences with MCMC.qpcr PrimEFF function
-## 5. Wrangle sample info a with dplyr packag
-## 6. Analyze data with MCMC.qpcr package
+## 5. create counts dataframe 
+## 6. Join count and sample info, sort by sample, order in logical fashion
+## 7. Turn cq into counts
+## 8. Mixed model
+## 9. Mixed model with diagnostic plots
+## 10. Plot the data!!
 
 #install.packages("xlsx", dependencies = TRUE) #note, must have Java installed on computer
 #install.packages("plyr")
@@ -27,7 +31,7 @@ library(plyr)
 library(MCMC.qpcr)
 library(reshape2)
 
-## 1. read raw data starting with row 42, then make quanitity a real number
+## 1. read raw data with a nice forloop, only 1 sheet, start row 42, only first 26 columsn, everything as character
 
 file_list <- list.files() #creates a string will all the files in a diretory for us to loop through
 
@@ -37,50 +41,99 @@ for (file in file_list){
   
   # if the merged dataset doesn't exist, create it
   if (!exists("rawdata")){
-    rawdata <- read.xlsx("2016-06-29_183933-mouse-RnRNA-tenfold-rmh.xls", sheetIndex = 1, startRow=42, stringsAsFactors=FALSE)
+    rawdata <- read.xlsx(file , sheetIndex = 1, startRow=42, colIndex = (1:26),  stringsAsFactors=FALSE)
     
   }
   
   # if the merged dataset does exist, append to it
   if (exists("rawdata")){
-    rawdata <- read.xlsx("2016-06-29_183933-mouse-RnRNA-tenfold-rmh.xls", sheetIndex = 1, startRow=42, stringsAsFactors=FALSE)
+    temp_dataset <- read.xlsx(file , sheetIndex = 1, startRow=42, colIndex = (1:26), stringsAsFactors=FALSE)
     rawdata<-rbind(rawdata, temp_dataset)
     rm(temp_dataset)
   }
   
 }
 
-
-
-
-rawdata <- read.xlsx("2016-06-29_183933-mouse-RnRNA-tenfold-rmh.xls", sheetIndex = 1, startRow=42, stringsAsFactors=FALSE)
 names(rawdata)
 str(rawdata)
-rawdata$Quantity <- as.numeric(rawdata$Quantity)
-str(rawdata$Quantity)
 
+## 2. Clean data
 
+cleandata <- rawdata
+cleandata <- rename(cleandata, c("Quantity"="dna", "CT"="cq", "Target.Name"="gene")) 
+names(cleandata)
+str(cleandata)
+cleandata$dna <- as.numeric(cleandata$dna, na.rm = TRUE)
+cleandata$cq <- as.numeric(cleandata$cq, na.rm = TRUE)
+cleandata$gene <- as.factor(cleandata$gene)
+str(cleandata)
 
-## 2. Create dilutions dataframe with quantiry, target name, and ct. 
-##    Then rename for MCMC.qpcr
-dilutions <- rawdata %>%
+## 3. Create dilutions dataframe with quantiry, target name, and ct. 
+dilutions <- cleandata %>%
   filter(Task == "STANDARD") %>%
-  select(Quantity, CT, Target.Name)
-dilutions <- rename(dilutions, c("Quantity"="dna", "CT"="cq", "Target.Name"="gene")) 
+  select(dna, cq, gene) 
 str(dilutions)
-
 
 
 ## 3. Calculate primer efficiences with MCMC.qpcr PrimEFF function
 PrimEff(dilutions) # makes a plot with the primer efficiencies
-eff <- PrimEff(dilutions) #creates a table with the primer efficiencies
-
+amp.eff <- PrimEff(dilutions) #creates a table with the primer efficiencies
 
 ## 4. create counts dataframe 
-counts <- rawdata %>%
+counts <- cleandata %>%
   filter(Task == "UNKNOWN") %>%
-  select(Sample.Name,Target.Name,CT)
-str(counts)
-counts <- rename(counts, c("Sample.Name"="sample", "Target.Name"="gene", "CT"="cq")) 
+  select(Well, sample, gene,  cq) %>%
+  dcast(Well + sample ~ gene )
 
 
+## 6. Join count and sample info, sort by sample, order in logical fashion
+samples <- read.csv("Z:/NSB_2016/IntegrativeNeuroscience/qPCR-mouse/Rayna/data_samples/sample_info.csv", header=TRUE, sep="," )
+
+data <- inner_join(counts, samples) %>%
+  arrange(sample) %>%
+  select(sample, condition, CbNaV, IH, inx1, inx2)
+
+## 7. Turn cq into counts
+
+dd=cq2counts(
+  data=data,
+  genecols=c(3:6), # where the Cq data are in the data table
+  condcols=c(1:2), # which columns contain factors
+  effic=amp.eff,
+  Cq1=37
+)
+head(dd)
+
+## 8. Mixed model
+mm=mcmc.qpcr(
+  fixed="condition",
+  random="sample",
+  data=dd
+)
+summary(mm)
+
+## 9. Mixed model with diagnostic plots
+mmd=mcmc.qpcr(
+  fixed="condition",
+  random="sample",
+  data=dd,
+  pr=T,
+  pl=T
+)
+
+diagnostic.mcmc(
+  model=mmd,
+  col="grey50",
+  cex=0.8
+)
+
+## 10. Plot the data!!
+
+HPDplot(
+  model=mm,
+  factors="conditionstg",
+  main="STG vs Muscle"
+)
+
+s1=HPDsummary(model=mm,data=dd)
+s0=HPDsummary(model=mm,data=dd,relative=TRUE)
