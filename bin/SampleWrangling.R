cbd83eaddfaee94138488d74051156f2c204ec80
Rayna Harris <rayna.harris@utexas.edu>
2016-07-21 14:43:24 -0400

    updated script

--- bin/SampleWrangling.R
+++ bin/SampleWrangling.R
@@ -17,10 +17,17 @@ library(reshape2)
 
 ## 1. read data
 setwd("~/Github/qPCR-mouse/data")
+setwd("Z:/NSB_2016/4_MouseMolecular/qPCR-mouse/data")
 punches <- read.csv("NSBpunches.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
 animals <- read.csv("NSBanimals.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
 punches$mouse <- as.factor(punches$Mouse)
 animals$mouse <- as.factor(animals$Mouse)
+
+
+# 2. melt slice data with atlas reference
+animalsmelt<- melt(animals,id.vars=c("mouse2","Genotype","Conflict","APA","Group"), measure.vars=c("Slice1","Slice2","Slice3","Slice4","Slice5","Slice6","Slice7","Slice8"),variable.name="Slice2",value.name="Atlas.location",na.rm=FALSE)
+animalsmelt$Slice2 <- gsub("Slice","",animalsmelt$Slice2)
+animalsmelt$Slice2 <- as.integer(animalsmelt$Slice2)
 
 
 ## join animals and punches using the mouse id
@@ -44,14 +51,28 @@ alldata$mouse.x <- NULL
 str(alldata)
 
 
+## FMR1 mice
+FMR1_mice <- alldata %>%
+  filter(Genotype %in% c("FMR1", "WT"))   %>%
+  distinct(Mouse, Genotype, Group, Punch)
+
+##
+ARmice <- alldata %>%
+  filter(Genotype %in% c("Htt (YAC128)", "WT (FVB/NJ)")) %>%
+  filter(APA %in% c("HomeCage", "NoShock")) %>%
+  filter(Punch %in% c("CA2", "DG")) %>%
+  distinct(Mouse, Genotype, APA, Group)
+
 ## 3. Group Project: FMR1 - conflict - slices
 FMR1_conflict_slices <- alldata %>%
-  filter(Punch == "slice", Genotype %in% c("WT", "FMR1"), 
+  filter(Punch == "slice", Genotype %in% c("WT", "FMR1"),  Group %in% c("FMR1 Conflict Trained", "WT Conflict Trained"),
          Conflict == "Conflict", Slice != "5", 
          Mouse != "16-119C", Mouse != "16-119D", Mouse != "16-118C", Mouse != "16-118D") %>%
-  select(Mouse, Group, Slice,Punch, Tube, storagebox) %>%
+  select(Mouse, Group, Slice,Punch, Tube, storagebox, Atlas.location) %>%
   arrange(Group, Mouse) 
 View(FMR1_conflict_slices) 
+#write.csv(FMR1_conflict_slices, "FMR1_conflict_slices.csv", quote=F, row.names = FALSE)
+
 
 FMR1_conflict_animals <- dcast(FMR1_conflict_slices, Mouse + Group ~ Slice, value.var="Slice")
 View(FMR1_conflict_animals)
@@ -79,3 +100,6 @@ HttTissues <- (count(HttTissues, vars=c("Group","Punch")))
 HttTissues <- spread(HttTissues, Punch, freq)
 
 
+CA2 <- alldata %>%
+  filter(Punch == "CA2") 
+CA2counts <- (count(CA2, vars=c("Group","Mouse", "Punch")))
